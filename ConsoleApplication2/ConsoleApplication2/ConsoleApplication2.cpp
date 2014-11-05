694a1e76e8a2179c0ef14f7ee657e25949081359
jiangxx <jiangxxonline@sina.com>
2014-11-05 11:27:56 +0800

    testNodePPT
    testNodePPT

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication2.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b;
+	a = 1;
+	b = a*a + 1;
+	cout << a + b;
+	return 0;
+}
